f3f3c4c784ccbef7bc2c491de517c045292303c7
Jacob Viti <thevitij64@gmail.com>
2020-08-13 04:37:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   def initialize(name, artist=nil, genre=nil)
     @name = name
     self.artist = artist
-    Artist.new(self.artist)
+    add_song(name)
     self.genre = genre
     save
   end
