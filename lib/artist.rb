d0b037a85a9c9ae167ecd6ad4343ac126be634c8
Jacob Viti <thevitij64@gmail.com>
2020-08-13 01:30:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,8 @@ class Artist
   end
 
   def songs
-    Song.all.select {|song| song.artist == self}
+    artistSongs = Song.all.select {|song| song.artist == self}
+    @songs << artistSongs
   end
 
 end
