b53567a4dbf9da13ee414c84abba8d12c5199549
Jacob Viti <thevitij64@gmail.com>
2020-08-13 05:19:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,10 +50,10 @@ class Song
       @genre
     end
 
-
+  
     def self.find_or_create_by_name(name)
   if self.find_by_name(name)
-    self.find_by_name(name)
+    return self.find_by_name(name)
   else
   self.create(name)
 end
