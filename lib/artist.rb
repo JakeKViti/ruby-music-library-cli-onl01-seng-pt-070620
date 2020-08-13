2755f1b942235f3417176e9a714c2a5c3d1862ea
Jacob Viti <thevitij64@gmail.com>
2020-08-13 03:59:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,10 +31,10 @@ class Artist
   end
 
   def add_song(song)
-    if @songs == song
-    @songs << song
-    song.artist = self
-  end
+    if @songs != song
+      @songs << song
+      song.artist = self
+    end
   end
 
 end
